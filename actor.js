2e6cebe645994fe0e8f43581efd70325b3e847c2
git <richard.james.grove@gmail.com>
2023-09-14 15:58:07 +0100

    html and css

--- actor.js
+++ actor.js
@@ -1,7 +1,25 @@
 //prompt
 
-let actor = prompt("Who is your favourite actor");
+let genre = prompt("Who is your favourite genre");
 
-let message = "Your favourite actor is" + actor;
+document.write ("Your favourite genre is"  + genre);
+
+if(genre <= horror) {
+    message = "red";
+} else if (genre < action )
+{
+    message = "Blue";
+} else if (genre < scifi )
+{
+    message = "green";
+}
+else if (genre < romantic )
+{
+    message = "pink";}
+
+    else if (genre < comedy )
+{
+        message = "orange"; }
+
 
 document.write(message);
--- name.js
+++ name.js
@@ -1,9 +1,44 @@
 
 // prompt 
 
+function name(){
 let username = prompt("What is your name?");
+console.log(username);
+document.write  ("Hey " + username);}
 
-let message = "Hey " + username;
 
+
+
+function movie() {
+let film = prompt("Who is your favourite film");
+
+let message = film + " Interesting choice of film";
+document.write(message);}
+
+
+
+
+function type(){
+let genre = prompt("What is your favourite genre");
+
+document.write (" Your favourite genre is " + genre); 
+
+if(genre <= horror) {
+    message = "red";
+} else if (genre < action )
+{
+    message = "Blue";
+} else if (genre < scifi )
+{
+    message = "green";
+}
+else if (genre < romantic )
+{
+    message = "pink";}
+
+    else if (genre < comedy )
+{
+        message = "orange"; }
+
+}
 document.write(message);
-
